fc50a193c913ed5b76b1100bc5502ff6a869393a
FloHiwg <Florian@Heiwig.com>
2020-01-12 16:21:37 +0100

    added small example of reward pages

--- src/navigation/Navigator.ts
+++ src/navigation/Navigator.ts
@@ -4,14 +4,15 @@ import HomeScreen from '../screens/Home';
 import LoadingScreen from '../screens/Loading';
 import OptionsScreen from '../screens/Options';
 import SettingsScreen from '../screens/Settings';
+import RewardsScreen from '../screens/Rewards';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createSwitchNavigator } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 
 const HomeStack = createStackNavigator(
-    { HomeScreen, DetailScreen, OptionsScreen},
-    { initialRouteName: "HomeScreen" }
+    { RewardsScreen, HomeScreen, DetailScreen, OptionsScreen},
+    { initialRouteName: "RewardsScreen" }
   );
 
 const MainNavigator = Platform.select({
